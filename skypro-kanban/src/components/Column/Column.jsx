09fd15be38556c59f77d25a293fca6e0435f12d7
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-05-20 17:33:13 +0800

    2 задание

--- skypro-kanban/src/components/Column/Column.jsx
+++ skypro-kanban/src/components/Column/Column.jsx
@@ -1,11 +1,15 @@
 import Card from "../Card/Card";
-
 const Column = ({ title }) => {
     return (           <div className="main__column column">
     <div className="column__title">
       <p>{title}</p>
     </div>
     <div className="cards">
+      <div>
+        {
+
+        }
+      </div>
       <Card category="Web Design" title="Задача 1" date="30.10.23" />
       <Card category="Research" title="Задача 2" date="30.10.23" />
       <Card category="Copywriting" title="Задача 3" date="30.10.23" />
--- skypro-kanban/src/components/Main/Main.jsx
+++ skypro-kanban/src/components/Main/Main.jsx
@@ -1,16 +1,29 @@
+import { cardList } from "../../data";
 import Column from "../Column/Column";
-
+const statusList = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
 const Main = () => {
     return (    <main className="main">
     <div className="container">
       
       <div className="main__block">
         <div className="main__content">
-          <Column title="Без статуса" /> 
-          <Column title="Нужно сделать" /> 
+          {statusList.map((status) => (
+              <Column 
+              key={status} 
+              title={status}
+              cardList={cardList.filter((card) => card.status === status)} /> 
+          ))}
+          {/* <Column title="Без статуса" />  */}
+          {/* <Column title="Нужно сделать" /> 
           <Column title="В работе" /> 
           <Column title="Тестирование" /> 
-          <Column title="Готово" /> 
+          <Column title="Готово" />  */}
         </div>
       
       </div>
