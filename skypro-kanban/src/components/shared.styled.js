1339daecf03890db452c33439492c3e44488f586
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-07-19 10:53:37 +0800

    shared.styled.js

---
+++ skypro-kanban/src/components/shared.styled.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+  `
+
+export {Container}
+=======
+export const Container = styled.div`
+max-width: 1260px;
+width: 100%;
+margin: 0 auto;
+padding: 0 30px;
+`;
+>>>>>>> f64bf37f92ec38d261ff75336d3f79217e7f8cbd
