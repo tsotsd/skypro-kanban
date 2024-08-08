f24f0b29e07c341c4bd20997623e0f3fbaece5e1
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-08-08 11:03:04 +0800

    PrivateRoute

--- skypro-kanban/src/App.jsx
+++ skypro-kanban/src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import PopBrowse from './components/PopUps/PopBrowse/PopBrowse'
-import PopExit from './components/PopUps/PopExit/PopExit'
-import PopNewCard from './components/PopUps/PopNewCard/PopNewCard'
-import { cardList } from './data'
-import {GlobalStyle} from './global.styled'
 import { Routes, Route } from 'react-router-dom';
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute" 
-import MainPage from "./pages/MainPage"
-import CardPage from "./pages/CardPage"
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute"; 
+import MainPage from './pages/MainPage';
+import CardPage from './pages/CardPage';
+
 
 function App() {
    const isAuth = true;
 
-   const [cards, setCards] = useState(cardList);
    return ( 
    <Routes>   
       <Route element={<PrivateRoute isAuth={isAuth} />}>
          <Route path={"/"} element={<MainPage />} />
          <Route path={"/card"} element={<CardPage />} />
-   <GlobalStyle/>
-   <div className="wrapper">
-    <PopExit />
-    <PopNewCard />
-    <PopBrowse />
-    <Header setCards={setCards} cards={cards}/>
-    <Main cardList={cards}/>
-   </div>
-   </Route>
+    </Route>
    </Routes>
    );
 }
 
-
-export default App
+export default App;
--- skypro-kanban/src/pages/MainPage.jsx
+++ skypro-kanban/src/pages/MainPage.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
 import './App.css'
+
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
-import PopBrowse from './components/PopUps/PopBrowse/PopBrowse'
-import PopExit from './components/PopUps/PopExit/PopExit'
-import PopNewCard from './components/PopUps/PopNewCard/PopNewCard'
+// import PopBrowse from './components/PopUps/PopBrowse/PopBrowse'
+// import PopExit from './components/PopUps/PopExit/PopExit'
+// import PopNewCard from './components/PopUps/PopNewCard/PopNewCard'
+import { useState } from 'react'
 import { cardList } from './data'
 import {GlobalStyle, Wrapper} from './global.styled'
 
 export default function MainPage() {
     const [cards, setCards] = useState(cardList);
+    
    return ( 
        <>
         <GlobalStyle/>
         <Wrapper>
-            <PopExit />
+            {/* <PopExit />
             <PopNewCard />
-            <PopBrowse />
+            <PopBrowse /> */}
             <Header setCards={setCards} cards={cards}/>
             <Main cardList={cards}/>
         </Wrapper>
