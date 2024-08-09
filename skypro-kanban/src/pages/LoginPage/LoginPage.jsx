dcfd0353543245392ccbd052c9eb4cba22105349
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-08-09 16:16:24 +0800

    wrapper

--- skypro-kanban/src/pages/LoginPage/LoginPage.jsx
+++ skypro-kanban/src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,15 @@
 import Login from "../../components/Login/Login";
 import { GlobalStyleSignIn, Wrapper } from "../../components/Login/Login.styled"
 
-export const LoginPage = ({ login }) => {
+export const LoginPage = ({login}) => {
     return (
-        <>
+           <>
             <GlobalStyleSignIn />
-            <Wrapper>
-                <Login login = { login } />
+            <Wrapper />
+            <Login login={login}/>
             <Wrapper/>
-        </>
-    );
-};
-
+            </>
+    )
+}
 
 export default LoginPage;
