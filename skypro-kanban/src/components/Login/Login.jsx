027435ada23a784edb80e762f0703b82c3a83fb0
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-08-09 11:23:25 +0800

    login.jsx

--- skypro-kanban/src/components/Login/Login.jsx
+++ skypro-kanban/src/components/Login/Login.jsx
@@ -1,5 +1,26 @@
 const Login = () => {
     return (
-
+        <div className="wrapper">
+            <div className="container-signin">
+                <div className="modal">
+                    <div className="modal__block">
+                        <div className="modal_ttl">
+                            <h2>Вход</h2>
+                        </div>
+                        <form className="modal__form-login" id="formLogin" action="#">
+                            <input className="modal__input" type="text" name="login" id="formlogin" placeholder="Эл. почта" />
+                            <input className="modal__input" type="passport" name="passport" id="formpassport" placeholder="Пароль" />
+                            <button className="modal__btn-enter _hover01" id="btnEnter">Войти</button>
+                            <div className="modal__form-group">
+                                <p>Нужно зарегистрироваться?</p>
+                                <Link to="/register">Регистрируйтесь здесь</Link>
+                            </div>    
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
     );
 };
+
+export default Login;
